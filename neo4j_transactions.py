f087f9a82e1ae6dca4eb9537fc2eb50c5f05742d
artemgur <artemgur01@gmail.com>
2023-05-26 18:34:58 +0300

    Added graph_owner_id property to nodes

--- neo4j_transactions.py
+++ neo4j_transactions.py
@@ -3,49 +3,53 @@ from typing import Iterable
 from neo4j import Transaction
 
 
-def add_user(tx: Transaction, user_id):
-    tx.run('CREATE (u:User {id: $id})', id=user_id)
+def add_user(tx: Transaction, user_id, graph_owner_id: int):
+    tx.run('CREATE (u:User {id: $id, graph_owner_id: $graph_owner_id})', id=user_id, graph_owner_id=graph_owner_id)
 
 
-def add_users(tx: Transaction, user_ids: set[int]):
+def add_users(tx: Transaction, user_ids: set[int], graph_owner_id: int):
     tx.run('''
     UNWIND $user_ids AS x
-    MERGE (u:User {id: x})
-    ''', user_ids=list(user_ids))
+    MERGE (u:User {id: x, graph_owner_id: $graph_owner_id})
+    ''', user_ids=list(user_ids), graph_owner_id=graph_owner_id)
 
 
-def add_user_connection(tx: Transaction, subscriber_from_id: int, subscriber_to_id: int, connection_type: str):
+def add_user_connection(tx: Transaction, subscriber_from_id: int, subscriber_to_id: int, connection_type: str, graph_owner_id: int):
     if connection_type not in ['FRIEND', 'FOLLOWER']:
         raise ValueError('Attempted to use unsupported connection_type')
     tx.run(f'''
     MATCH (a: User), (b: User)
     WHERE a.id = $subscriber_from_id AND b.id = $subscriber_to_id
+      AND a.graph_owner_id = $graph_owner_id AND b.graph_owner_id = $graph_owner_id
     CREATE (a) -[:{connection_type}]-> (b)
-    ''', subscriber_from_id=subscriber_from_id, subscriber_to_id=subscriber_to_id)
+    ''', subscriber_from_id=subscriber_from_id, subscriber_to_id=subscriber_to_id, graph_owner_id=graph_owner_id)
 
 
-def add_friendships(tx: Transaction, user_id: int, friends_ids: list[int]):
+def add_friendships(tx: Transaction, user_id: int, friends_ids: list[int], graph_owner_id: int):
     tx.run('''
     MATCH (a: User), (b: User)
     WHERE a.id = $user_id AND b.id IN $friends_ids
+      AND a.graph_owner_id = $graph_owner_id AND b.graph_owner_id = $graph_owner_id
     CREATE (a) -[:FRIEND]-> (b)
-    ''', user_id=user_id, friends_ids=friends_ids)
+    ''', user_id=user_id, friends_ids=friends_ids, graph_owner_id=graph_owner_id)
 
 
-def add_followers(tx: Transaction, followers_ids: list[int], user_id: int):
+def add_followers(tx: Transaction, followers_ids: list[int], user_id: int, graph_owner_id: int):
     tx.run('''
     MATCH (a: User), (b: User)
     WHERE a.id = $user_id AND b.id IN $followers_ids
+      AND a.graph_owner_id = $graph_owner_id AND b.graph_owner_id = $graph_owner_id
     CREATE (a) <-[:FOLLOWER]- (b)
-    ''', user_id=user_id, followers_ids=followers_ids)
+    ''', user_id=user_id, followers_ids=followers_ids, graph_owner_id=graph_owner_id)
 
 
-def fix_one_directional_friendships(tx: Transaction):
+def fix_one_directional_friendships(tx: Transaction, graph_owner_id: int):
     tx.run(f'''
     MATCH (a:User) -[:FRIEND]-> (b:User)
     WHERE NOT (a) <-[:FRIEND]- (b)
+      AND a.graph_owner_id = $graph_owner_id AND b.graph_owner_id = $graph_owner_id
     CREATE (a) <-[:FRIEND]- (b)
-    ''')
+    ''', graph_owner_id=graph_owner_id)
 
 
 # def get_user(tx: Transaction, user_id):
@@ -86,3 +90,10 @@ def get_nodes(tx: Transaction) -> list[dict]:
     RETURN properties(a)
     ''')
     return [record.values()[0] for record in result]
+
+
+def clear_graph_by_owner(tx: Transaction, graph_owner_id: int):
+    tx.run('''
+    MATCH (n:User {graph_owner_id: $graph_owner_id})
+    DETACH DELETE n
+    ''', graph_owner_id=graph_owner_id)
