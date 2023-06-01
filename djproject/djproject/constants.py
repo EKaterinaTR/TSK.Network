f0c0e78ff08fe9fc2bebb1354ae50e5079b4cd47
artemgur <artemgur01@gmail.com>
2023-06-01 14:48:11 +0300

    Moved test VK api key to _development

--- djproject/djproject/constants.py
+++ djproject/djproject/constants.py
@@ -1,5 +1,3 @@
-VK_API_KEY = '32797754327977543279775425320fcc36332793279775452389e04a9aeaa9a29fcfab7'
-
 NEO4J_CONNECTION_PARAMETERS_CLOUD = {'uri': 'neo4j+s://cc201494.databases.neo4j.io',
                                     'auth': ('neo4j', 'mZPyuC0xBty-UxfkTtAmRNH6cBhhuhWVsGs1MP2ztT8')}
 NEO4J_CONNECTION_PARAMETERS_LOCAL = {'uri': 'neo4j://localhost',
